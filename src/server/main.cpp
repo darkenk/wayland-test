c7c99e0b25875010ad10fb3f503e79d1a5f72ec1
Dariusz Kluska <darkenk@gmail.com>
2015-09-23 22:23:44 +0200

    Add simple rendering loop (it's dumb, bot working)
    Signed-off-by: Dariusz Kluska <darkenk@gmail.com>

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -59,24 +59,17 @@ public:
         mInterface.set_buffer_scale = WaylandSurface::surfaceSetBufferScale;
 
         mResource = wl_resource_create(client, &wl_surface_interface,
-            wl_resource_get_version(resource), id);
+                                       wl_resource_get_version(resource), id);
         if (not mResource) {
             wl_resource_post_no_memory(resource);
             return;
         }
         wl_resource_set_implementation(mResource, &mInterface, this, nullptr);
-        // TODO: dumb hardcoding!
-        mOutput = new X11Backend(480, 360);
+        mIsReady = false;
     }
 
     void surfaceCommit(wl_client* client, wl_resource* resource) {
-        wl_shm_buffer* buf = wl_shm_buffer_get(mBuffer);
-        wl_shm_buffer_begin_access(buf);
-        uint32_t* color = (uint32_t*)wl_shm_buffer_get_data(buf);
-        mOutput->drawBuffer(color);
-        wl_resource_queue_event(mBuffer, WL_BUFFER_RELEASE);
-        wl_callback_send_done(mCallbackDone, 0 /*dumb timestamp*/);
-        wl_shm_buffer_end_access(buf);
+        mIsReady = true;
     }
 
     void surfaceAttach(wl_client* client, wl_resource* resource, wl_resource* buffer,
@@ -86,12 +79,29 @@ public:
         wl_shm_buffer_begin_access(buf);
         mHeight = wl_shm_buffer_get_height(buf);
         mWidth = wl_shm_buffer_get_width(buf);
-        mOutput->setSize(mWidth, mHeight);
         wl_shm_buffer_end_access(buf);
     }
 
     void surfaceFrame(wl_client* client, wl_resource* resource, uint32_t callback) {
         mCallbackDone = wl_resource_create(client, &wl_callback_interface, 1, callback);
+    }
+
+    bool isReady() {
+        return mIsReady;
+    }
+
+    uint8_t* lockBuffer() {
+        wl_shm_buffer* buf = wl_shm_buffer_get(mBuffer);
+        wl_shm_buffer_begin_access(buf);
+        return (uint8_t*)wl_shm_buffer_get_data(buf);
+    }
+
+    void unlockBuffer() {
+        wl_shm_buffer* buf = wl_shm_buffer_get(mBuffer);
+        wl_resource_queue_event(mBuffer, WL_BUFFER_RELEASE);
+        wl_callback_send_done(mCallbackDone, 0 /*dumb timestamp*/);
+        wl_shm_buffer_end_access(buf);
+        mIsReady = false;
     }
 
 private:
@@ -101,7 +111,7 @@ private:
     wl_resource* mCallbackDone;
     int32_t mWidth;
     int32_t mHeight;
-    X11Backend* mOutput;
+    bool mIsReady;
 
     static void surfaceDestroy(wl_client* client, wl_resource* resource) {
         LOGVP();
@@ -155,9 +165,16 @@ private:
 class WaylandCompositor
 {
 public:
-    WaylandCompositor() {
+    WaylandCompositor(wl_display* display, X11Backend* output) {
         mInterface.create_surface = WaylandCompositor::hookCreateSurface;
         mInterface.create_region = WaylandCompositor::hookCreateRegion;
+
+        // repaint handler
+        wl_event_loop* loop = wl_display_get_event_loop(display);
+        mRepaintTimer = wl_event_loop_add_timer(loop, WaylandCompositor::hookRepaintHandler, this);
+        wl_event_source_timer_update(mRepaintTimer, REPAINT_DELAY);
+
+        mOutput = output;
     }
 
     struct wl_compositor_interface& getInterface() {
@@ -180,10 +197,23 @@ public:
         mRegion = make_unique<WaylandRegion>(client, resource, id);
     }
 
+    int repaint() {
+        LOGVP();
+        if (mSurface && mSurface->isReady()) {
+            mOutput->drawBuffer(mSurface->lockBuffer());
+            mSurface->unlockBuffer();
+        }
+        wl_event_source_timer_update(mRepaintTimer, REPAINT_DELAY);
+        return 1;
+    }
+
 private:
     struct wl_compositor_interface mInterface;
     unique_ptr<WaylandSurface> mSurface;
     unique_ptr<WaylandRegion> mRegion;
+    wl_event_source* mRepaintTimer;
+    X11Backend* mOutput;
+    static constexpr int REPAINT_DELAY = 16;
 
     static void hookCreateSurface(wl_client* client, wl_resource* resource, uint32_t id) {
         LOGVP();
@@ -199,6 +229,11 @@ private:
                     wl_resource_get_user_data(resource));
         wc->createRegion(client, resource, id);
     }
+
+    static int hookRepaintHandler(void* data) {
+        WaylandCompositor* wc = reinterpret_cast<WaylandCompositor*>(data);
+        return wc->repaint();
+    }
 };
 
 class WaylandServer
@@ -209,16 +244,16 @@ public:
         if (not mDisplay) {
             throw exception();
         }
-        mLoop = wl_display_get_event_loop(mDisplay);
+        mOutput = new X11Backend(480, 360);
+        mOutput->setWaylandDisplay(mDisplay);
+        mCompositor = make_unique<WaylandCompositor>(mDisplay, mOutput);
         const char* socketName = wl_display_add_socket_auto(mDisplay);
         LOGVP("Socket Name %s", socketName);
-        if (not wl_global_create(mDisplay, &wl_compositor_interface, 3, &mCompositor,
+        if (not wl_global_create(mDisplay, &wl_compositor_interface, 3, mCompositor.get(),
                                  WaylandServer::bindCompositor)) {
             throw exception();
         }
         wl_display_init_shm(mDisplay);
-        mOutput = new X11Backend(480, 360);
-        mOutput->setWaylandDisplay(mDisplay);
     }
 
     ~WaylandServer() {
@@ -235,9 +270,8 @@ public:
 
 private:
     wl_display* mDisplay;
-    wl_event_loop* mLoop;
     wl_global* mWlCompositor;
-    WaylandCompositor mCompositor;
+    unique_ptr<WaylandCompositor> mCompositor;
     X11Backend* mOutput;
 
     static void bindCompositor(wl_client* client, void* data, uint32_t version, uint32_t id) {
--- src/server/x11backend.hpp
+++ src/server/x11backend.hpp
@@ -14,14 +14,15 @@ public:
     X11Backend(uint32_t width, uint32_t height) {
         mWidth = 480;
         mHeight = 360;
+        mWlDisplay = nullptr;
         mDisplay = XOpenDisplay(NULL);
         if (not mDisplay) {
             Exception(__FUNCTION__);
         }
         mRoot = DefaultRootWindow(mDisplay);
         mWindow = XCreateSimpleWindow(mDisplay, mRoot, 0, 0, mWidth, mHeight, 0, 0, 0);
-        WM_DELETE_WINDOW = XInternAtom(mDisplay, "WM_DELETE_WINDOW", False);
-        XSetWMProtocols(mDisplay, mWindow, &WM_DELETE_WINDOW, 1);
+        mWmDeleteWindow = XInternAtom(mDisplay, "WM_DELETE_WINDOW", False);
+        XSetWMProtocols(mDisplay, mWindow, &mWmDeleteWindow, 1);
         XMapWindow(mDisplay, mWindow);
         XFlush(mDisplay);
         mXcbConnection = XGetXCBConnection(mDisplay);
@@ -55,7 +56,7 @@ public:
         mHeight = height;
     }
 
-    void drawBuffer(uint32_t* buffer) {
+    void drawBuffer(uint8_t* buffer) {
         mImage->data = reinterpret_cast<char*>(buffer);
         XPutImage(mDisplay, mPixmap, mGraphicContext, mImage, 0, 0, 0, 0, mWidth, mHeight);
         XSetWindowBackgroundPixmap(mDisplay, mWindow, mPixmap);
@@ -65,33 +66,13 @@ public:
     }
 
     void setWaylandDisplay(wl_display* display) {
+        if (mWlDisplay) {
+            throw Exception("Wayland display already set");
+        }
         mWlDisplay = display;
         mXcbSource = wl_event_loop_add_fd(wl_display_get_event_loop(display), xcb_get_file_descriptor(mXcbConnection), WL_EVENT_READABLE,
                              X11Backend::hookHandleX11Events, this);
         wl_event_source_check(mXcbSource);
-    }
-
-    int handleX11Events() {
-        xcb_generic_event_t* event = xcb_poll_for_event(mXcbConnection);
-        if (not event) {
-            return 0;
-        }
-        switch(event->response_type & ~0x80) {
-        case XCB_CLIENT_MESSAGE:
-            if (((xcb_client_message_event_t*) event)->data.data32[0] == WM_DELETE_WINDOW) {
-                LOGVP("Destroy me");
-                wl_event_source_remove(mXcbSource);
-                XDestroyWindow(mDisplay, mWindow);
-                XFlush(mDisplay);
-                wl_display_terminate(mWlDisplay);
-            }
-            break;
-        default:
-            LOGVP("Not handled %d", event->response_type & ~0x80);
-            break;
-        }
-        free(event);
-        return 1;
     }
 
 private:
@@ -106,11 +87,34 @@ private:
     XImage* mImage;
     wl_event_source* mXcbSource;
     xcb_connection_t* mXcbConnection;
-    Atom WM_DELETE_WINDOW;
+    Atom mWmDeleteWindow;
     wl_display* mWlDisplay;
 
     static int hookHandleX11Events(int fd, uint32_t mask, void* data) {
         return reinterpret_cast<X11Backend*>(data)->handleX11Events();
+    }
+
+    int handleX11Events() {
+        xcb_generic_event_t* event = xcb_poll_for_event(mXcbConnection);
+        if (not event) {
+            return 0;
+        }
+        switch(event->response_type & ~0x80) {
+        case XCB_CLIENT_MESSAGE:
+            if (((xcb_client_message_event_t*) event)->data.data32[0] == mWmDeleteWindow) {
+                LOGVP("Destroy me");
+                wl_event_source_remove(mXcbSource);
+                XDestroyWindow(mDisplay, mWindow);
+                XFlush(mDisplay);
+                wl_display_terminate(mWlDisplay);
+            }
+            break;
+        default:
+            LOGVP("Not handled %d", event->response_type & ~0x80);
+            break;
+        }
+        free(event);
+        return 1;
     }
 };
 
