0dd58d8218cea16de91b337aa2d3cf59bf20c7fd
Dariusz Kluska <darkenk@gmail.com>
2015-10-04 19:42:10 +0200

    Simple shm client
    Signed-off-by: Dariusz Kluska <darkenk@gmail.com>

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,70 +2,206 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "../utils/logger.hpp"
+#include "../utils/exceptions.hpp"
+#include "../utils/make_unique.hpp"
 
-wl_compositor* compositor = nullptr;
-wl_shell* shell = nullptr;
+using namespace std;
 
-static void global_registry_handler(void *data, struct wl_registry *registry, uint32_t id,
-    const char *interface, uint32_t version)
+class WaylandBuffer
 {
-    printf("Got a registry event for %s id %d\n", interface, id);
-    if (strcmp(interface, wl_compositor_interface.name) == 0) {
-        compositor = (wl_compositor*)wl_registry_bind(registry, id, &wl_compositor_interface, 1);
-    } else if (strcmp(interface, wl_shell_interface.name) == 0) {
-        shell = (wl_shell*)wl_registry_bind(registry, id, &wl_shell_interface, 1);
+public:
+    WaylandBuffer(wl_shm* shm, int width, int height) {
+        mBufferListener.release = hookBufferRelease;
+        mBuffer = createShmBuffer(shm, width, height);
+        wl_buffer_add_listener(mBuffer, &mBufferListener, this);
+        mReadyToDraw = true;
     }
-}
+    virtual ~WaylandBuffer() {}
 
-static void global_registry_remover(void *data, struct wl_registry *registry, uint32_t id)
+    uint8_t* data() {
+        return mData;
+    }
+
+    wl_buffer* buffer() {
+        return mBuffer;
+    }
+
+    bool readyToDraw() {
+        return mReadyToDraw;
+    }
+
+    void setReadyToDraw(bool ready) {
+        mReadyToDraw = ready;
+    }
+
+private:
+    wl_buffer* mBuffer;
+    wl_buffer_listener mBufferListener;
+    bool mReadyToDraw;
+    uint8_t* mData;
+
+    wl_buffer* createShmBuffer(wl_shm* shm, int width, int height) {
+        int stride = width * 4;
+        int size = stride * height;
+        int fd = createAnonymousFile(size);
+        mData = reinterpret_cast<uint8_t*>(mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED,
+                                                fd, 0));
+        if (mData == MAP_FAILED) {
+            close(fd);
+            throw Exception("Cannot create shm buffer");
+        }
+        wl_shm_pool* pool = wl_shm_create_pool(shm, fd, size);
+        wl_buffer* buff = wl_shm_pool_create_buffer(pool, 0, width, height, stride,
+                                                    WL_SHM_FORMAT_XRGB8888);
+        wl_shm_pool_destroy(pool);
+        close(fd);
+        return buff;
+    }
+
+    void release(wl_buffer* buffer) {
+        LOGVP("");
+        mReadyToDraw = true;
+    }
+
+    static int createAnonymousFile(off_t size) {
+        char tmpname[] = "XXXXXX";
+        int fd = mkostemp(tmpname, O_CLOEXEC);
+        if (fd >= 0) {
+            unlink(tmpname);
+        } else {
+            throw Exception("Cannot create anonymous file");
+        }
+        if (posix_fallocate(fd, 0, size) != 0) {
+            close(fd);
+            throw Exception("Cannot allocate anonymous file");
+        }
+        return fd;
+    }
+
+    static void hookBufferRelease(void *data, struct wl_buffer *buffer) {
+        reinterpret_cast<WaylandBuffer*>(data)->release(buffer);
+    }
+};
+
+class WaylandSurface
 {
-    printf("Got a registry losing event for %d\n", id);
-}
+public:
+    WaylandSurface(wl_compositor* compositor, wl_shm* sharedMemory):
+        mSharedMemory(sharedMemory), mWidth(480), mHeight(360) {
+        mFrameListener.done = hookFrameDone;
+        mSurface = wl_compositor_create_surface(compositor);
+        mBuffer = make_unique<WaylandBuffer>(mSharedMemory, mWidth, mHeight);
+        redraw(nullptr, 0);
+    }
 
-static const wl_registry_listener registry_listener = {
-    global_registry_handler,
-    global_registry_remover
+    virtual ~WaylandSurface() {}
+
+    void redraw(wl_callback* callback, uint32_t time) {
+        if (not mBuffer->readyToDraw()) {
+            return;
+        }
+        // just paint something white
+        static uint8_t color = 0x00;
+        color++;
+        uint8_t* data =  mBuffer->data();
+        memset(data, color, mWidth * mHeight * 4);
+
+        wl_surface_attach(mSurface, mBuffer->buffer(), 0, 0);
+        wl_surface_damage(mSurface, 0, 0, mWidth, mHeight);
+        if (callback) {
+            wl_callback_destroy(callback);
+        }
+        mFrameCallback = wl_surface_frame(mSurface);
+        wl_callback_add_listener(mFrameCallback, &mFrameListener, this);
+        wl_surface_commit(mSurface);
+        mBuffer->setReadyToDraw(false);
+    }
+
+private:
+    wl_shm* mSharedMemory;
+    wl_surface* mSurface;
+    wl_callback* mFrameCallback;
+    wl_callback_listener mFrameListener;
+    unique_ptr<WaylandBuffer> mBuffer;
+    int mWidth;
+    int mHeight;
+
+    static void hookFrameDone(void* data, wl_callback* callback, uint32_t time) {
+        reinterpret_cast<WaylandSurface*>(data)->redraw(callback, time);
+    }
+
+};
+
+class WaylandClient
+{
+public:
+    WaylandClient() : mDisplay(nullptr),
+        mCompositor(nullptr), mSharedMemory(nullptr), mSurface(nullptr) {
+        mRegistryListener.global = WaylandClient::registryHandler;
+        mRegistryListener.global_remove = WaylandClient::registryRemover;
+        mDisplay = wl_display_connect(nullptr);
+        if (not mDisplay) {
+            throw Exception("Error opening display");
+        }
+
+        wl_registry *registry = wl_display_get_registry(mDisplay);
+        wl_registry_add_listener(registry, &mRegistryListener, this);
+
+        wl_display_dispatch(mDisplay);
+        wl_display_roundtrip(mDisplay); // wait synchronously for event
+
+        if (not (mCompositor && mSharedMemory)) {
+            throw Exception("cannot find wl_compositor or wl_shm");
+        }
+    }
+
+    void createSurface() {
+        mSurface = make_unique<WaylandSurface>(mCompositor, mSharedMemory);
+    }
+
+    virtual ~WaylandClient() {
+        wl_display_disconnect(mDisplay);
+    }
+
+    void run() {
+        createSurface();
+        while(wl_display_dispatch(mDisplay)) {};
+    }
+
+private:
+    wl_display* mDisplay;
+    wl_compositor* mCompositor;
+    wl_shm* mSharedMemory;
+    wl_registry_listener mRegistryListener;
+    unique_ptr<WaylandSurface> mSurface;
+
+    void registryHandler(wl_registry *registry, uint32_t id, const char *interface,
+                         uint32_t version) {
+        LOGVP("Got a registry event for %s id %d\n", interface, id);
+        if (strcmp(interface, wl_compositor_interface.name) == 0) {
+            mCompositor = (wl_compositor*)wl_registry_bind(registry, id, &wl_compositor_interface, 1);
+        } else if (strcmp(interface, wl_shm_interface.name) == 0) {
+            mSharedMemory = (wl_shm*)wl_registry_bind(registry, id, &wl_shm_interface, 1);
+        }
+    }
+
+    static void registryHandler(void *data, wl_registry *registry, uint32_t id,
+                                const char *interface, uint32_t version) {
+        reinterpret_cast<WaylandClient*>(data)->registryHandler(registry, id, interface, version);
+    }
+
+    static void registryRemover(void *data, struct wl_registry *registry, uint32_t id) {
+        LOGVP("Got a registry losing event for %d\n", id);
+    }
+
 };
 
 int main(int argc, char *argv[])
 {
-    wl_display *display = wl_display_connect(nullptr);
-    if (not display) {
-        printf("Error opening display");
-        exit(1);
-    }
-
-    wl_registry *registry = wl_display_get_registry(display);
-    wl_registry_add_listener(registry, &registry_listener, nullptr);
-
-    wl_display_dispatch(display);
-    wl_display_roundtrip(display); // wait synchronously for event
-
-    if (not compositor) {
-        printf("Can't find compositor\n");
-        exit(1);
-    } else {
-        printf("Found compositor\n");
-    }
-
-    wl_surface* surface = wl_compositor_create_surface(compositor);
-    if (not surface) {
-        printf("Can't create surface\n");
-        exit(1);
-    } else {
-        printf("Created surface\n");
-    }
-
-    wl_shell_surface* shell_surface = wl_shell_get_shell_surface(shell, surface);
-    if (not shell_surface) {
-        printf("Can't create shell surface\n");
-        exit(1);
-    } else {
-        printf("Created shell surface\n");
-    }
-    wl_shell_surface_set_toplevel(shell_surface);
-
-    wl_display_disconnect(display);
-
+    WaylandClient().run();
     return 0;
 }
