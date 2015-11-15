c2b28dbabe68d534e6a401158539fd5d8e3fd796
Dariusz Kluska <darkenk@gmail.com>
2015-11-15 20:03:07 +0100

    Initial duble buffering for wl_surface
    Signed-off-by: Dariusz Kluska <darkenk@gmail.com>

--- src/server/waylandsurface.hpp
+++ src/server/waylandsurface.hpp
@@ -5,7 +5,21 @@
 #include <memory>
 #include <utility>
 #include "../utils/logger.hpp"
+#include "../utils/make_unique.hpp"
 #include "waylandshellsurface.hpp"
+
+class SurfaceState
+{
+public:
+    SurfaceState(): buffer(nullptr), x(0), y(0), callbackDone(nullptr), isReady(false) {}
+
+    wl_resource* buffer;
+    int32_t x;
+    int32_t y;
+    wl_resource* callbackDone;
+    bool isReady;
+};
+
 
 class WaylandSurface
 {
@@ -14,59 +28,71 @@ public:
         /*mX(0),*/ mY(0) {
         mResource = wl_resource_create(client, &wl_surface_interface,
                                        wl_resource_get_version(resource), id);
-
-        mBuffer = nullptr;
-        mCallbackDone = nullptr;
         if (not mResource) {
             wl_resource_post_no_memory(resource);
             return;
         }
         wl_resource_set_implementation(mResource, &sInterface, this, nullptr);
-        mIsReady = false;
         mClient = client;
+        mFront = std::make_unique<SurfaceState>();
+        mBack = std::make_unique<SurfaceState>();
     }
 
     void surfaceCommit(wl_client* /*client*/, wl_resource* /*resource*/) {
-        if (mBuffer) {
-            mIsReady = true;
+        if (mBack->buffer) {
+            mBack->isReady = true;
+            mBuffersFilled++;
+            if (mBuffersFilled < MAX_BUFFERS) {
+                wl_callback_send_done(mBack->callbackDone, 0 /*dumb timestamp*/);
+                mBack->callbackDone = nullptr;
+            }
+            swapBuffers();
         }
     }
 
     void surfaceAttach(wl_client* /*client*/, wl_resource* /*resource*/, wl_resource* buffer,
-                       int32_t /*sx*/, int32_t /*sy*/) {
+                       int32_t x, int32_t y) {
         wl_shm_buffer* buf = wl_shm_buffer_get(buffer);
         if (not buf) {
             LOGVP("Why buffer is empty?");
             return;
         }
-        mBuffer = buffer;
-        wl_shm_buffer_begin_access(buf);
-        mHeight = wl_shm_buffer_get_height(buf);
-        mWidth = wl_shm_buffer_get_width(buf);
-        wl_shm_buffer_end_access(buf);
+        mBack->x = x;
+        mBack->y = y;
+        mBack->buffer = buffer;
     }
 
     void surfaceFrame(wl_client* client, wl_resource* /*resource*/, uint32_t callback) {
-        mCallbackDone = wl_resource_create(client, &wl_callback_interface, 1, callback);
+        mBack->callbackDone = wl_resource_create(client, &wl_callback_interface, 1, callback);
     }
 
     bool isReady() {
-        return mIsReady;
+        return mFront->isReady;
     }
 
     wl_shm_buffer* lockBuffer() {
-        wl_shm_buffer* buf = wl_shm_buffer_get(mBuffer);
+        if (not mFront->isReady) {
+            return nullptr;
+        }
+        wl_shm_buffer* buf = wl_shm_buffer_get(mFront->buffer);
         wl_shm_buffer_begin_access(buf);
         return buf;
     }
 
     void unlockBuffer() {
-        wl_shm_buffer* buf = wl_shm_buffer_get(mBuffer);
-        wl_resource_queue_event(mBuffer, WL_BUFFER_RELEASE);
-        wl_callback_send_done(mCallbackDone, 0 /*dumb timestamp*/);
+        if (not mFront->buffer) {
+            return;
+        }
+        wl_shm_buffer* buf = wl_shm_buffer_get(mFront->buffer);
+        wl_resource_queue_event(mFront->buffer, WL_BUFFER_RELEASE);
         wl_shm_buffer_end_access(buf);
-        mBuffer = nullptr;
-        mIsReady = false;
+        mFront->buffer = nullptr;
+        if (mFront->callbackDone) {
+            wl_callback_send_done(mFront->callbackDone, 0 /*dumb timestamp*/);
+            mFront->callbackDone = nullptr;
+        }
+        mFront->isReady = false;
+        mBuffersFilled--;
     }
 
     wl_client* client() const {
@@ -92,15 +118,18 @@ public:
 private:
     static struct wl_surface_interface sInterface;
     wl_resource* mResource;
-    wl_resource* mBuffer;
-    wl_resource* mCallbackDone;
-    int32_t mWidth;
-    int32_t mHeight;
-    bool mIsReady;
+    std::unique_ptr<SurfaceState> mFront;
+    std::unique_ptr<SurfaceState> mBack;
+    uint32_t mBuffersFilled = 0;
+    static constexpr uint32_t MAX_BUFFERS  = 2;
     wl_client* mClient;
     //int mX;
     int mY;
     std::unique_ptr<WaylandShellSurface> mShellSurface;
+
+    void swapBuffers() {
+        mFront.swap(mBack);
+    }
 
     static void surfaceDestroy(wl_client* /*client*/, wl_resource* resource) {
         LOGVP();
